491385b7104ce7b9b1b59ea0e71a15d9005d67df
Jay Lim <jlimoman@gmail.com>
2023-04-19 21:39:55 -0600

    complete arr find method lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+// function superbowlWin(array) {
+//   const win = array.find(function(obj) {
+//     obj.result === 'W';
+//   })
+//   if (win) {
+//     return win.year;
+//   }
+//   return undefined;
+// }
+
+
+// function superbowlWin(array) {
+//   const win = array.find(obj => obj.result === 'W');
+//   return win ? win.year : undefined;
+// }
+
+const superbowlWin = array => {
+  const win = array.find(obj => obj.result === 'W');
+  return win ? win.year : undefined;
+}
